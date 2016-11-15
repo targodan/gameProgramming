4902b9f544ff152302b91f7f165eb2b938c6535e
Tim Stadtmann <tim.stadtmann@rwth-aachen.de>
2016-11-15 23:59:34 +0100

    Add first version of main-loop

--- engine/src/Game.h
+++ engine/src/Game.h
@@ -24,8 +24,7 @@ namespace engine {
         virtual void render() = 0;
         virtual void update() = 0;
         virtual void processEvents() = 0;
-        
-        
+    
     public:
         Game(int argc, char** argv, double ups);
         Game(const Game& orig) = delete;

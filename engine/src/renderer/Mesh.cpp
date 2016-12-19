3914c7227b4602b85501a41d266440953c5abb45
Luca Corbatto <luca@corbatto.de>
2016-12-19 11:08:40 +0100

    Changed behaviour of Mesh class

--- engine/src/renderer/Mesh.cpp
+++ engine/src/renderer/Mesh.cpp
@@ -3,45 +3,76 @@
 namespace engine {
     namespace renderer {
         Mesh::Mesh(Array<Vertex> vertices, Array<GLuint> indices, GLenum usage) 
-            : vertices(vertices), indices(indices)
-        {
-            this->nVertices = vertices.size();
-            this->initMesh();
-        }   
+            : vertices(vertices), indices(indices), wasLoaded(false) {}
+        
+        Mesh::Mesh(const Mesh& orig)
+            : usage(orig.usage), vertices(orig.vertices), indices(orig.indices), wasLoaded(false) {}
+        
+        Mesh::Mesh(Mesh&& orig)
+            : usage(std::move(orig.usage)), vao(std::move(orig.vao)), vbo(std::move(orig.vbo)),
+                vertices(std::move(orig.vertices)), indices(std::move(orig.indices)), wasLoaded(std::move(orig.wasLoaded)) {}
 
         Mesh::~Mesh() {
             this->releaseMesh();
         }
         
-        void Mesh::initMesh() {
-            // Generate and bind VBO & VAO
-            glGenVertexArrays(1, &this->vao);
-            glGenBuffers(1, &this->vbo);
+        Mesh& Mesh::operator=(const Mesh& right) {
+            this->releaseMesh();
             
-            glBindVertexArray(this->vao);
-            glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
-            
-            // Store data in bound buffer
-            glBufferData(GL_ARRAY_BUFFER, (GLint) sizeof(Vertex)*(this->nVertices), &this->vertices, this->usage);
-            
-            // Specify vertex attributes for VAO in bound vertex array
-            glEnableVertexAttribArray(Vertex::posLoc);
-            glVertexAttribPointer(Vertex::posLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, position));
-            
-            glEnableVertexAttribArray(Vertex::normLoc);
-            glVertexAttribPointer(Vertex::normLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, normal));
-            
-            glEnableVertexAttribArray(Vertex::texLoc);
-            glVertexAttribPointer(Vertex::texLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, textureCoord));
-            
-            // Unbind buffers
-            glBindBuffer(GL_ARRAY_BUFFER, 0);
-            glBindVertexArray(0);
+            this->usage = right.usage;
+            this->vao = right.vao;
+            this->vbo = right.vbo;
+            this->vertices = right.vertices;
+            this->indices = right.indices;
+            this->wasLoaded = false;
+            return *this;
+        }
+        
+        Mesh& Mesh::operator=(Mesh&& right) {
+            std::swap(this->usage, right.usage);
+            std::swap(this->vao, right.vao);
+            std::swap(this->vbo, right.vbo);
+            std::swap(this->vertices, right.vertices);
+            std::swap(this->indices, right.indices);
+            std::swap(this->wasLoaded, right.wasLoaded);
+            return *this;
+        }
+        
+        void Mesh::loadMesh() {
+            if(!this->wasLoaded) {
+                // Generate and bind VBO & VAO
+                glGenVertexArrays(1, &this->vao);
+                glGenBuffers(1, &this->vbo);
+
+                glBindVertexArray(this->vao);
+                glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+
+                // Store data in bound buffer
+                glBufferData(GL_ARRAY_BUFFER, (GLint) sizeof(Vertex)*(this->vertices.size()), &this->vertices, this->usage);
+
+                // Specify vertex attributes for VAO in bound vertex array
+                glEnableVertexAttribArray(Vertex::posLoc);
+                glVertexAttribPointer(Vertex::posLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, position));
+
+                glEnableVertexAttribArray(Vertex::normLoc);
+                glVertexAttribPointer(Vertex::normLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, normal));
+
+                glEnableVertexAttribArray(Vertex::texLoc);
+                glVertexAttribPointer(Vertex::texLoc, 3, GL_FLOAT, GL_FALSE, (GLint) sizeof(Vertex), (GLvoid*) offsetof(Vertex, textureCoord));
+
+                // Unbind buffers
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+                glBindVertexArray(0);
+                
+                this->wasLoaded = true;
+            }
         }
         
         void Mesh::releaseMesh() {
-            glDeleteBuffers(1, &this->vbo);
-            glDeleteVertexArrays(1, &this->vao);
+            if(this->wasLoaded) {
+                glDeleteBuffers(1, &this->vbo);
+                glDeleteVertexArrays(1, &this->vao);
+            }
         }
     }
 }
--- engine/src/renderer/Mesh.h
+++ engine/src/renderer/Mesh.h
@@ -1,7 +1,7 @@
 #ifndef MESH_H
 #define MESH_H
 
-#include "util/Array.h"
+#include "../util/Array.h"
 #include "Vertex.h"
 #include "gl/gl_core_3_3.h"
 
@@ -10,25 +10,39 @@ namespace engine {
         using namespace util;
         using namespace gl;
         
+        /**
+         * Mesh holds a mesh and handles loading it into the graphics card.
+         * NOTE:    The mesh does NOT get loaded into the graphics card automatically,
+         *          but it DOES get released automatically.
+         *          Following this, the copy of an already loaded mesh does NOT
+         *          get loaded automatically either.
+         */
         class Mesh {
         public:
             Mesh(Array<Vertex> vertices, Array<GLuint> indices, GLenum usage = GL_STATIC_DRAW);
-            Mesh(const Mesh& orig) = delete;
+            
+            Mesh(const Mesh& orig);
+            Mesh(Mesh&& orig);
+
+            Mesh& operator=(const Mesh& right);
+            Mesh& operator=(Mesh&& right);
+            
             virtual ~Mesh();
             
             std::string getUsage() const;
+            
+            void loadMesh();
+            void releaseMesh();
             
         private:
             GLenum usage;
             
             GLuint vao, vbo;
-            std::size_t nVertices;
             
-            const Array<Vertex> vertices;
-            const Array<GLuint> indices;
+            Array<Vertex> vertices;
+            Array<GLuint> indices;
             
-            void initMesh();
-            void releaseMesh();
+            bool wasLoaded;
         };
     }
 }

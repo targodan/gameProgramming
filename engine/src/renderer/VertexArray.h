89ba0e0a674c0b9e5ceeb3e5c5680e3a913f50c9
Tim Stadtmann <tim.stadtmann@rwth-aachen.de>
2016-12-29 00:12:01 +0100

    Improvements in VBO & VAO wrappers
    Also, added loadData-methods

--- engine/src/renderer/VertexArray.h
+++ engine/src/renderer/VertexArray.h
@@ -5,35 +5,142 @@
 #include "VertexBuffer.h"
 #include "gl/gl_core_3_3.h"
 #include <memory>
+#include "util/vector.h"
+#include <list>
 
 namespace engine {
     namespace renderer {
+        using util::vector;
+        
         class VertexArray {
         public:
-            VertexArray();
-            VertexArray(const VertexArray& orig) = delete;
-            virtual ~VertexArray();
+            VertexArray() 
+                : bound(false) {
+                this->generateVertexArray();
+            }
             
-            void setAttributePointers();
+            VertexArray(const std::list<std::unique_ptr<VertexBuffer>> vbos) 
+                : vbos(vbos), bound(false) {
+                    this->generateVertexArray();
+            }
+                
+            VertexArray(const VertexArray& orig) 
+                : vbos(orig.vbos), id(orig.id), bound(orig.bound) {
+                // Note: the copy points to the exact same vao on the graphics card
+            }
+                
+            VertexArray(VertexArray&& orig) 
+                : vbos(std::move(orig.vbos)), id(std::move(orig.id)), bound(std::move(orig.bound)) {
+                // Note: I don't know if this is necessary. When this function returns,
+                //       will orig be deleted? If so, its deletion would release its
+                //       buffer. Therefore, a new buffer has to be generated.
+                
+                orig.releaseVertexArray();
+                this->generateVertexArray();
+            }
+
+            VertexArray& operator=(const VertexArray& right) {
+                this->vbos = right.vbos;
+                this->id = right.id;
+                this->bound = right.bound;
+                
+                return *this;
+            }
+            VertexArray& operator=(VertexArray&& right) {
+                this->vbos = std::move(right.vbos);
+                this->bound = std::move(right.bound);
+                
+                right.releaseVertexArray();
+                this->generateVertexArray();
+                
+                return *this;
+            }
             
-            void bind() const;
-            void unbind() const; // TODO: make static
+            ~VertexArray() {
+                this->releaseVertexArray();
+            }
             
-            void enableAttribute(GLuint index);
-            void disableAttribute(GLuint index);
+            void generateVertexArray() {
+                glGenVertexArrays(1, &(this->id));
+            }
+            void releaseVertexArray() {
+                glDeleteVertexArrays(1, &(this->id));
+            }
             
-            static const bool isBound() {
-                return bound;
+            void loadData() {
+                for(auto& vbo : this->vbos) {
+                    vbo->bind();
+                    vbo->loadData();
+                    vbo->bind();
+                }
+            }
+            
+            void setAttributePointers() {
+                for(auto& vbo : this->vbos) {
+                    vbo->bind();
+                    setAttributePointers(vbo->getAttributes());
+                    vbo->unbind();
+                }
+            }
+            
+            void attachVBO(std::unique_ptr<VertexBuffer> vbo) {
+                vbos.push_back(std::move(vbo));
+            }
+            
+            void bind() const {
+                if(this->bound) {
+                    return;
+                } else if(VertexArray::anyVAOBound) {
+                    // TODO: Log warning; maybe bindBuffer to 0?
+                    // No error - rebind buffer to this vao
+                    VertexArray::anyVAOBound = false; 
+                }
+
+                glBindVertexArray(this->id);
+                VertexArray::anyVAOBound = true;
+            }
+            void unbind() const {
+                if(!this->bound) {
+                    return;
+                }
+
+                glBindVertexArray(0);
+                VertexArray::anyVAOBound = false;
+            }
+            
+            bool isBound() const {
+                return this->bound;
+            }
+            static bool isAnyVAOBound() {
+                return anyVAOBound;
             }
         private:
-            void setAttributePointers(const std::list<VertexAttribute>& attributes);
-            void setAttributePointer(const VertexAttribute& attribute);
+            void enableAttribute(GLuint index) {
+                glEnableVertexAttribArray(index);
+            }
+            void disableAttribute(GLuint index) {
+                glDisableVertexAttribArray(index);
+            }
+            
+            void setAttributePointers(const std::list<VertexAttribute>& attributes) {
+                for(auto attribute : attributes) {
+                    setAttributePointer(attribute);
+                    enableAttribute(attribute.index);
+                }
+            }
+            void setAttributePointer(const VertexAttribute& attribute) {
+                glVertexAttribPointer(attribute.index, attribute.size, attribute.type, 
+                    attribute.normalized, attribute.stride, attribute.offset);
+            }
             
             std::list<std::unique_ptr<VertexBuffer>> vbos;
             GLuint id;
             
-            static bool bound;
+            bool bound;
+            static bool anyVAOBound;
         };
+        
+        bool VertexArray::anyVAOBound = false;
     }
 }
 
--- engine/src/renderer/VertexBuffer.h
+++ engine/src/renderer/VertexBuffer.h
@@ -2,35 +2,129 @@
 #define VERTEXBUFFER_H
 
 #include "VertexAttribute.h"
+#include "DataUsagePattern.h"
 #include "gl/gl_core_3_3.h"
+#include "WTFException.h"
+#include "util/vector.h"
 #include <list>
 
 namespace engine {
     namespace renderer {
         using namespace gl;
+        using util::vector;
         
+        struct BufferData {
+            const void* dataPtr;
+            size_t size;
+            DataUsagePattern usage;
+        };
+        
+        /*
+         * VertexBuffer is a simple wrapper for VBOs. It does NOT hold the data
+         * to render, but rather handles it -> it sets up the space on the 
+         * graphics card, saves how the graphics card should interpret the data,
+         * can load the data to it and finally release the space again.
+         */
         class VertexBuffer {
         public:
-            VertexBuffer();
-            VertexBuffer(const VertexBuffer& orig) = delete;
-            virtual ~VertexBuffer();
+            VertexBuffer() 
+                : data {nullptr, 0, DataUsagePattern::STATIC_DRAW}, bound(false) {
+                this->generateBuffer();
+            }
             
-            const GLuint* getIDPtr() const;
-            void bind() const;
-            void unbind() const;  // TODO: make static?
-            void setAttributes(const std::list<VertexAttribute>& attribs);
-            const std::list<VertexAttribute>& getAttributes() const;
+            VertexBuffer(const void* dataPtr, size_t size, DataUsagePattern usage) 
+                : data {dataPtr, size, usage}, bound(false) {
+                this->generateBuffer();
+            }
+                
+            VertexBuffer(const VertexBuffer& orig) 
+                : id(orig.id), data(orig.data), attributes(orig.attributes), bound(orig.bound) {
+                // Note: the copy points to the exact same buffer on the graphics card
+            }
             
-            static const bool isBound() {
-                return bound;
+            VertexBuffer(VertexBuffer&& orig) 
+                : id(std::move(orig.id)), data(std::move(orig.data)), 
+                  attributes(std::move(orig.attributes)), bound(std::move(orig.bound)) {
+                // Note: I don't know if this is necessary. When this function returns,
+                //       will orig be deleted? If so, its deletion would release its
+                //       buffer. Therefore, a new buffer has to be generated.
+                
+                orig.releaseBuffer();
+                this->generateBuffer();
+            }
+            
+            ~VertexBuffer() {
+                this->releaseBuffer();
+            }
+            
+            void generateBuffer() {
+                glGenBuffers(1, &(this->id));
+            }
+            void releaseBuffer() {
+                glDeleteBuffers(1, &(this->id));
+            }
+
+            void setAttributes(const std::list<VertexAttribute> attribs) {
+                this->attributes = attribs;
+            }
+            const std::list<VertexAttribute>& getAttributes() const {
+                return this->attributes;
+            }
+            
+            void loadData() {
+                if(!this->data.dataPtr) {
+                    throw WTFException("Could not buffer data: No data specified");
+                }
+                
+                glBufferData(GL_ARRAY_BUFFER, data.size, data.dataPtr, data.usage);
+            }
+            void loadData(const void* data, size_t size, DataUsagePattern usage) {
+                glBufferData(GL_ARRAY_BUFFER, size, data, usage);
+                this->data = {data, size, usage};
+            }
+            
+            
+            GLuint getID() const {
+                return this->id;
+            }
+            
+            void bind() const {
+                if(this->bound) {
+                    return;
+                } else if(VertexBuffer::anyVBOBound) {
+                    // TODO: Log warning; maybe bindBuffer to 0?
+                    VertexBuffer::anyVBOBound = false;
+                }
+            
+                glBindBuffer(GL_ARRAY_BUFFER, this->id);
+                VertexBuffer::anyVBOBound = true;
+            }
+            void unbind() const {
+                if(!this->bound) {
+                    return;
+                }
+
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+                VertexBuffer::anyVBOBound = false;
+            }
+            
+            bool isBound() const {
+                return this->bound;
+            }
+            
+            static bool isAnyVBOBound() {
+                return anyVBOBound;
             }
         private:
             GLuint id;
+            BufferData data;
+            std::list<VertexAttribute> attributes;
             
-            std::list<VertexAttribute> attributes; 
-            
-            static bool bound;
-        };        
+            bool bound;
+            static bool anyVBOBound;
+        };
+        
+        bool VertexBuffer::anyVBOBound = false;
     }
 }
 

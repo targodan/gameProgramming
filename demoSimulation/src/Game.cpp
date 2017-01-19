c0d53abcf68effe8f71a1626d1f972332ca0add8
Luca Corbatto <luca@corbatto.de>
2017-01-19 11:45:03 +0100

    SQUASH ME

--- demoSimulation/src/Game.cpp
+++ demoSimulation/src/Game.cpp
@@ -1,7 +1,31 @@
 #include "Game.h"
 
+#include "engine/renderer/Mesh.h"
+#include "engine/ECSCommon.h"
+
+using namespace engine;
+using namespace engine::renderer;
+using namespace engine::ECSCommon;
+
 namespace demoSimulation {
     void Game::initialize() {
+        this->systemManager.enableSystem<PlacementSystem>();
+        this->systemManager.enableSystem<RenderSystem>();
+                
+        Vertex frontBottom({0, 0, 0.5});
+        Vertex backLeft({-0.5, 0, -0.5});
+        Vertex backRight(0.5, 0, -0.5);
+        Vertex up(0, 1, 0);
+        Mesh tetrahedronMesh({frontBottom, backRight, backLeft, up}, {0, 1, 3, 2, 0, 3, 1, 2, 3});
+        tetrahedronMesh.loadMesh();
         
+        Material material(std::make_shared<ShaderProgram>("src/triangle_sh.vsh", 
+                                                         "src/triangle_sh.fsh"));
+        
+        this->tetrahedron = this->entityManager.createEntity("Tetrahedron")
+                .addComponent<VisualComponent>(tetrahedronMesh, material)
+                .addComponent<PlacementComponent>();
+        
+        engine::Game::initialize();
     }
 }
--- demoSimulation/src/Game.h
+++ demoSimulation/src/Game.h
@@ -3,10 +3,16 @@
 
 #include "engine/Game.h"
 #include "engine/physics/DeformableBody.h"
+#include "engine/ECS/Entity.h"
 
 namespace demoSimulation {
     class Game : public engine::Game {
+    protected:
+        engine::ECS::Entity tetrahedron;
+        
     public:
+        Game(int argc, char** argv, double ups=60) : engine::Game(argc, argv, ups) {}
+        
         virtual void initialize() override;
     };
 }
